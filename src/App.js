d99d9a9776758d69011737d1530cf27760b5b520
Justin Sitter <justin@jaytria.com>
2018-05-25 07:48:06 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tweet from './Tweet.js'
+import './Tweet.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header >
+        <header>
           <h1 className="title">DON'T PANIC</h1>
+
           <Tweet/>
         </header>
 
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './Tweet.css';
+import {Button, Icon, Modal} from 'react-materialize';
 
 class Tweet extends Component {
 constructor(props){
@@ -19,10 +21,25 @@ componentWillMount(){
 }
 render(){
     return(
-        <div>
+        <div className="tweet">
             <p>
             {this.state.tweet}
             </p>
+
+            <div className="btn-container">
+              <div>
+              <Button waves='light' btn-small onClick={this.fetchQuote.bind(this)} className="new-s">
+                New Sentence
+              </Button>
+              </div>
+
+              <Modal
+
+                header='Tweet to your fans!'
+                trigger={<Button waves='light' className="modal-btn">Tweet This!</Button>}>
+                <p>This feature is not implemented yet. Come back again soon!</p>
+              </Modal>
+            </div>
         </div>
     )
 }
